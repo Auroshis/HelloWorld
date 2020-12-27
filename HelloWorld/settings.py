0782c02f04451484dd5c83bf35a458b20a59c3eb
Auroshis <rayauroshis@gmail.com>
2020-12-27 23:15:50 +0530

    finetuning

--- HelloWorld/settings.py
+++ HelloWorld/settings.py
@@ -116,6 +116,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'polls/static')
+    os.path.join(BASE_DIR, './polls/static')
 ]
 django_heroku.settings(locals())
