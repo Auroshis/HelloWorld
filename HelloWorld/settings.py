f576cd66e5a87ba914ba9e0b81fe6afc8135984b
Auroshis <rayauroshis@gmail.com>
2020-12-28 23:04:55 +0530

    renaming app back to polls

--- HelloWorld/settings.py
+++ HelloWorld/settings.py
@@ -22,7 +22,7 @@ ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'core.apps.CoreConfig',
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
